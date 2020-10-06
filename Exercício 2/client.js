f2887638f97b15469cec5b023316f4e0156bc724
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-10-05 21:10:28 -0300

    feat: Exercício 2

---
+++ Exercício 2/client.js
@@ -0,0 +1,40 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("chatp2p.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const chatp2p = grpcObject.chatp2p;
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Choose the port you want to receive in\n", function (clientPort) {
+  rl.question("Choose the port you want to send to\n", function (serverPort) {
+    const server = new grpc.Server();
+    server.bind(
+      `localhost:${clientPort}`,
+      grpc.ServerCredentials.createInsecure()
+    );
+
+    server.addService(chatp2p.Chatp2p.service, {
+      sendMessage: receiveMessage,
+    });
+    server.start();
+
+    const client = new chatp2p.Chatp2p(
+      `localhost:${serverPort}`,
+      grpc.credentials.createInsecure()
+    );
+
+    function receiveMessage(call, callback) {
+      console.log(call.request.message);
+    }
+
+    rl.addListener("line", (line) => {
+      client.sendMessage({ message: line }, (err, response) => {
+      });
+    });
+  });
+});
