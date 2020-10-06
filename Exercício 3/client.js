0bc3899ba6370c5b12a5ade1124f5883b520da96
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-10-05 22:34:33 -0300

    feat: Exercício 3

--- Exercício 3/client.js
+++ Exercício 3/client.js
@@ -1,6 +1,32 @@
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
-const packageDef = protoLoader.loadSync("chatp2p.proto", {});
+const packageDef = protoLoader.loadSync("groupChat.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const groupChat = grpcObject.groupChat;
 const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const client = new groupChat.GroupChat(
+  "localhost:7474",
+  grpc.credentials.createInsecure()
+);
+rl.question("Choose your name\n", function (name) {
+  const call = client.login({ name });
+  call.on("data", (item) => {
+    console.log(`${item.name}: ${item.message}`);
+  });
+
+  console.log("Now send your messages, or send LOGOUT to end.");
+  rl.addListener("line", (line) => {
+    if (line !== "LOGOUT") {
+      client.sendMessage({ message: line }, (err, response) => {});
+    } else {
+      client.logout({}, (err, response) => {});
+      rl.close()
+    }
+  });
+});
--- Exercício 3/server.js
+++ Exercício 3/server.js
@@ -1,5 +1,54 @@
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
-const packageDef = protoLoader.loadSync("chatp2p.proto", {});
+const packageDef = protoLoader.loadSync("groupChat.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const groupChat = grpcObject.groupChat;
+
+const users = {};
+
+const server = new grpc.Server();
+server.bind("localhost:7474", grpc.ServerCredentials.createInsecure());
+
+server.addService(groupChat.GroupChat.service, {
+  sendMessage: handleMessage,
+  login: handleLogin,
+  logout: handleLogout,
+});
+server.start();
+
+function handleMessage(call, callback) {
+  for (const [peer, user] of Object.entries(users)) {
+    if (peer != call.getPeer()) {
+      user.call.write({
+        name: users[call.getPeer()].name,
+        message: call.request.message,
+      });
+    }
+  }
+  callback({});
+}
+
+function handleLogin(call, callback) {
+  users[call.getPeer()] = {
+    name: call.request.name,
+    call: call,
+  };
+  call.write({
+    name: "Server",
+    message: `There are currently ${Object.keys(users).length-1} other users in the chat!`,
+  });
+}
+
+function handleLogout(call, callback) {
+  users[call.getPeer()].call.end();
+  for (const [peer, user] of Object.entries(users)) {
+    if (peer != call.getPeer()) {
+      user.call.write({
+        name: users[call.getPeer()].name,
+        message: "LOGGED OUT",
+      });
+    }
+  }
+  delete users[call.getPeer()]
+  callback();
+}
